e3fcd5cfcfc772677032cdf3809a36f27cef957f
adaldin <aledaldin@gmail.com>
2021-10-24 16:00:24 +0200

    Corrección de rotación de tetromino i (-h) y actualización de funciones

--- script.js
+++ script.js
@@ -1,63 +1,76 @@
 // -------------------------------------------
-// CREACION DEL BOARD PRINCIPAL Y SECUNDARIO
+// CREACION DEL BOARD PRINCIPAL
 // -------------------------------------------
-// creación variables del ancho y alto del tablero
-// Los valores que le he puesto son los que nos ha dicho Alex en las instrucciones
 const BOARD_WIDTH = 10; //ANCHO EN Nº BLOQUES
 const BOARD_HEIGHT = 20; //ALTO EN Nº BLOQUES
 
+// -------------------------------------------
+// CREACION DEL BOARD SECUNDARIO
+// -------------------------------------------
+const BOARD_WIDTH_MINI_BOARD = 4;
+const BOARD_HEIGHT_MINI_BOARD = 4;
 
 
 // Creo función para crear un bloque
 function generateBoardBlock() {
-    // creo div
+
     const bloque = document.createElement('div');
-    // anñado clase del css (ver CSS section bloques/game__blocks)
     bloque.classList.add('game__blocks');
-    // este div lo creo para incluirlo dentro de bloque más grande
-    const bloqueInterior = document.createElement('div');
-    // anñado clase del css (ver section bloques/game__blocks--interior)
+    const bloqueInterior = document.createElement('div'); // este div lo creo para incluirlo dentro de bloque más grande
     bloqueInterior.classList.add('game__blocks--interior');
-    // al bloque padre le inyecto el bloque interior
     bloque.appendChild(bloqueInterior);
-    // al bloque padre le doy estilo (ancho y alto)
     bloque.style.width = '25px';
     bloque.style.height = '25px';
-    // devuelvo bloque
     return bloque;
+}
+
+
+// creacion de funcion para generar bloques para meterlos en la última fila
+function lastRow() {
+    const blockNoDisplay = document.createElement('div');
+    blockNoDisplay.classList.add('taken');
+    blockNoDisplay.style.width = '25px';
+    blockNoDisplay.style.height = '25px';
+    blockNoDisplay.style.display = 'none'; // tienen display none para que no sean visibles en el navegador
+    return blockNoDisplay;
+}
+
+// creacion de funcion para pintar los bloques creados en lastRow en la última fila
+function drawLastRow(clase, ancho) {
+    for (let i = 0; i < ancho; i++) {
+        document.querySelector(clase).appendChild(lastRow());
+    }
+
 }
 
 // creamos la función para pintar los bloques en el board.
 function drawBoard(containerClass, width, heigth) {
-    // uso un for para recorrer todo el contenedor y que se pinten todos los bloques
-    for (let i = 0; i < width * heigth; i++) {
+
+    for (let i = 0; i < width * heigth; i++) { // uso un for para recorrer todo el contenedor y que se pinten todos los bloques
 
         document.querySelector(containerClass).appendChild(generateBoardBlock());
     }
 }
-//ejecuto función que genera dentro del contenedor definido en 
-// '.game__board' (string) todos los bloques de hijos necesarios para
-// cumplir BOARD_WIDTH (number) y BOARD_HEIGHT (number).
-drawBoard('.game__board', BOARD_WIDTH, BOARD_HEIGHT);
-// ejecuto la funcion para rellenar instrucciones (ver CSS section aside/.next-tetro__board )
-drawBoard('.next-tetro__board', 4, 4);
+
+drawBoard('.game__board', BOARD_WIDTH, BOARD_HEIGHT); //ejecuto la funcion para el container principal 
+
+drawBoard('.next-tetro__board', BOARD_WIDTH_MINI_BOARD, BOARD_HEIGHT_MINI_BOARD); // ejecuto la funcion para rellenar el contenedor de instrucciones
+
+drawLastRow('.game__board', BOARD_WIDTH); // ejecutamos la funcion para pintar la última fila
 
 // -------------------------------------------
 // CREACION DE VARIABLES PARA IMPLEMENTAR
 // -------------------------------------------
-// creación de variable para seleccionar y traer board del index
+
 const GRID = document.querySelectorAll('.game__board');
-// creación de variable para seleccionar y traer cuadraditos hijos del board
-//con Array() cada div tendrá un específico index 
-const BOARD = Array(document.querySelectorAll('.game__board .game__blocks'));
+
+const BOARD = Array.from(document.querySelectorAll('.game__board .game__blocks'));
 const MINI_BOARD = Array.from(document.querySelectorAll('.next-tetro__board .game__blocks'))
-    //creacionn de variable para traer el div de puntuación
-const SCORE_DISPLAY = document.querySelector('#score__board');
-// creacion de variable para traer boton start
-const START_BTN = document.querySelector('start-button');
+
+const SCORE_DISPLAY = document.querySelector('#score__board'); //creacionn de variable para traer el div de puntuación
 
 // -------------------------------------------
-// CREACION DE PIEZAS L - J
+// CREACION DE TETROMINOS
 // -------------------------------------------
 const J_TETROMINO = [
     //ROTACION 0  
@@ -82,7 +95,7 @@ const L_TETROMINO = [
 const I_TETROMINO = [
     [1, BOARD_WIDTH + 1, BOARD_WIDTH * 2 + 1, BOARD_WIDTH * 3 + 1],
     [BOARD_WIDTH, BOARD_WIDTH + 1, BOARD_WIDTH + 2, BOARD_WIDTH + 3],
-    [1, BOARD_WIDTH + 1, BOARD_WIDTHh + 2, BOARD_WIDTH * 3 + 1],
+    [1, BOARD_WIDTH + 1, BOARD_WIDTH + 2, BOARD_WIDTH * 3 + 1],
     [BOARD_WIDTH, BOARD_WIDTH + 1, BOARD_WIDTH + 2, BOARD_WIDTH + 3]
 ];
 const T_TETROMINO = [
@@ -107,5 +120,45 @@ const S_TETROMINO = [
 ];
 
 
-// AGRUPACION DE TODAS LAS PIEZAS
+// CREACION DE ARRAY DE TODAS LAS PIEZAS
 const TETROMINOES = [J_TETROMINO, L_TETROMINO, I_TETROMINO, T_TETROMINO, S_TETROMINO, Z_TETROMINO];
+//DIFERENCIACION DE PIEZA QUE CAE
+let currentPosition = 4;
+let currentRotation = 0;
+
+// SELECCION RANDOM DE TETROMINO 
+let generateRandomTetrominoe = Math.floor(Math.random() * TETROMINOES.length);
+
+// SELECCION RANDOM DE INDEX DE TETROMINO 
+let currentTetromino = TETROMINOES[generateRandomTetrominoe][currentRotation];
+
+// PINTAR TETROMINO SELECCIONADO EN PANTALLA
+function drawTetrominoeInMainBoard() {
+    //en arr CurrenTetromino aplico for each
+    currentTetromino.forEach(index => {
+        //para que en cada iteración en la posicion de cada iteracion
+        //agregue la clase (opacidad)al div del grid tetromino
+        BOARD[currentPosition + index].classList.add('tetromino');
+    });
+}
+drawTetrominoeInMainBoard();
+
+// DESPINTAR DE TETROMINO SELECCIONADO EN PANTALLA
+function undrawTetrominoeInMainBoard() {
+    currentTetromino.forEach(index => {
+        BOARD[currentPosition + index].classList.remove('tetromino');
+    })
+}
+
+// -------------------------------------------
+// CREACION DE MOVIMIENTOS DE TETROMINOS
+// -------------------------------------------
+// creacion de constante de tiempo de caida de piezas 
+const TIMER = setInterval(moveDown, 1000);
+
+// creacion de funcion callback  moveDown de interval para que bajen
+function moveDown() {
+    undrawTetrominoeInMainBoard()
+    currentPosition += BOARD_WIDTH;
+    drawTetrominoeInMainBoard()
+}
